c961e4cda7b2303d13e076d33d54dcf4eb569490
Victor Polyakov <polyakov.victor21@gmail.com>
2024-03-24 16:39:19 +0200

    add start, end pages and delay

--- script_wa.py
+++ script_wa.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
@@ -12,8 +13,9 @@ print('Please enter test URL:')
 base_url = input()
 
 
-def save_question_and_answer(question, answer, filename='questions_and_answers.txt'):
+def save_question_and_answer(question, answer, page_number, filename='questions_and_answers.txt'):
     with open(filename, 'a', encoding='utf-8') as file:
+        file.write(str(page_number) + '\n')
         file.write(question + '\n')
         file.write('\n'.join(answer) + '\n\n')
 
@@ -34,19 +36,20 @@ def get_question_and_answer(soup):
     return question, answer_options
 
 
-def send_question_to_chatGPT(question, options):
+def send_question_to_chatGPT(question, options, page_number):
     prompt = " ".join([question, 'Варіанти відповідей:'] + options)
-    save_question_and_answer(question=question, answer=options)
+    save_question_and_answer(question=question, answer=options, page_number=page_number)
     print("Вопрос:", prompt)
 
 
 def main():
-    page_number = 0
+    start_page = int(input('Enter start page:'))
+    finish_page = int(input('Enter finish page:'))
     seen_questions = set()
-    while page_number < 5:
-        page_number += 1
-        url = f'{base_url}&page={page_number}'
-
+    while start_page <= finish_page:
+        time.sleep(1)
+        url = f'{base_url}&page={start_page-1}'
+        print(url)
         response = requests.get(url, cookies=cookies, allow_redirects=True)
 
         # Проверяем успешность запроса
@@ -61,11 +64,12 @@ def main():
                 break
             else:
                 seen_questions.add(question)
-                send_question_to_chatGPT(question, answer_options)
+                send_question_to_chatGPT(question, answer_options, start_page)
         else:
             print("Ошибка при получении страницы:", response.status_code)
             break
 
+        start_page += 1
 
 if __name__ == "__main__":
     main()
